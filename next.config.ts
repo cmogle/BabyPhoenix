ef473646ad8bbe9e04a444e97e9ed0a553cd4c9e
Conor M Ogle <conorogle@gmail.com>
2026-03-27 12:11:39 +0400

    Externalize postgres for server runtime

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["postgres"],
+  experimental: {
+    viewTransition: true,
+  },
 };
 
 export default nextConfig;
